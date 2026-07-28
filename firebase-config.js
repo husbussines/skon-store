376810554b043a61d7df5c167d0b874fb5234e4c
husbussines <husbussines@gmail.com>
2026-07-28 08:14:47 +0300

    Create firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,6 @@
+// ضع إعدادات Firebase Web App هنا بعد إنشاء المشروع.
+// سيبدأ المتجر عندها بحفظ الطلبات في مجموعة orders.
+window.FIREBASE_CONFIG = null;
+
+// مثال:
+// window.FIREBASE_CONFIG = { apiKey: "...", authDomain: "...", projectId: "...", appId: "..." };
