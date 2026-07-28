265e263df9a09bcbacadff61a2f7d0435ef4b475
husbussines <husbussines@gmail.com>
2026-07-28 08:24:31 +0300

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,6 +1,9 @@
-// ضع إعدادات Firebase Web App هنا بعد إنشاء المشروع.
-// سيبدأ المتجر عندها بحفظ الطلبات في مجموعة orders.
-window.FIREBASE_CONFIG = null;
-
-// مثال:
-// window.FIREBASE_CONFIG = { apiKey: "...", authDomain: "...", projectId: "...", appId: "..." };
+// إعدادات مشروع سُكُون في Firebase.
+window.FIREBASE_CONFIG = {
+  apiKey: "AIzaSyBKwmJPTiovoREWoZpnSbpp55neIvLLnLw",
+  authDomain: "skon-store-43437.firebaseapp.com",
+  projectId: "skon-store-43437",
+  storageBucket: "skon-store-43437.firebasestorage.app",
+  messagingSenderId: "6470983126",
+  appId: "1:6470983126:web:64d8bd0f3dad4f762cd877"
+};
